e7bf883726e1c4bee20be0e144516ca419b355fd
Robert Carr <carrenator@gmail.com>
2012-06-29 10:04:08 -0700

    Added userdata parsing and fixed data bag updates

--- cookbooks/demohelper/recipes/default.rb
+++ cookbooks/demohelper/recipes/default.rb
@@ -7,26 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 
-begin
-# Need to grab the demo from userdata or file
-customer_data = data_bag_item("user", "demo")
-rescue
-end
-
-
-user_data = Hash['demo_name' => 'demo1']
-server_role = 'other'
+params = Hash.new
 
 ohai_data = node.current_automatic
 cloud_data = ohai_data['cloud']
-cloud_data.merge!({'server_role'=>server_role})
 
-
-# TODO(ROB): Add to get userdata or file information from openstack as well
-unless cloud_data.nil? 
-if cloud_data['provider']= 'ec2'
-        cloud_data['userdata'] = ohai_data['ec2']['userdata']
+unless ohai_data['ec2']['userdata'].nil? then
+cloud_data['userdata'] = ohai_data['ec2']['userdata']
+  cloud_data['userdata'].split("&").each do |item|
+    item = item.split("=")
+    params[item[0]] = item[1].chomp
+  end
 end
+
+puts params.inspect
+
+cloud_data.merge!({'server_role'=>params['server_role'], 'launched' => Time.now })
+
+begin
+# Need to grab the demo from userdata or file
+customer_data = data_bag_item(params['userid'], "demo")
+rescue
 end
 
 # Update the databags with the relvant network information.  This will allow the other services
@@ -34,7 +35,7 @@ end
 
 unless customer_data.nil? || cloud_data.nil? 
   customer_data['demos'].each do |demo|
-    if demo['name'] == user_data['demo_name'] then
+    if demo['name'] == params['demo_name'] then
             demo['servers'] << cloud_data
     end
     customer_data.save
